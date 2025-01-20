ac74bc84d3482384bfc0b1663050d50956ba106e
Agume_kenneth <agumekenneth83@gmail.com>
2025-01-20 12:54:43 +0300

    learning

---
+++ basics.js
@@ -0,0 +1,48 @@
+// String datatype
+'',""
+let firstName ='Kenneth';
+let lastName = 'kwimire';
+let middleName ='Welcome back ${Last}';
+
+console.log(firstName + lastName);// length of a string
+console.log(firstName.toUpperCase());// length of a string
+
+//Boolean Datatypes 
+let isStudent = true;
+let hasGraduated = false;
+ console.log(isStudent && hasGraduated);//and logical operator
+ console.log(isStudent || hasGraduated);//or logiacal operator
+
+ let numberList =[1,2,3,4,5,6]
+ let numberListTwo =[6,7,4,8,9]
+ let fruitList =['apples','mangoes','berries']
+ console.log(numberList[3]);
+ console.log(fruitList);
+
+ fruitList.push('grapes');
+ console.log(fruitList);
+
+ fruitList.pop();
+ console.log(fruitList);
+
+ let personObject ={
+    "name": "kenneth",
+    'gender': "male"
+ }
+console.log(personObject.gender);
+
+//Arithmetic operators
+let numberOne =5;
+let numberTwo = 7;
+console.log(numberOne * numberTwo);
+console.log(numberOne + numberTwo);
+console.log(numberOne / numberTwo);
+
+//Comparison operators
+let numberSix = 2;
+let numberSeven ='2';
+
+console.log(numberSix == numberSeven);
+console.log(numberSix === numberSeven);
+
+
---
+++ controlflows.js
@@ -0,0 +1,39 @@
+/*
+Multi-
+Line
+Comment
+*/
+
+// Variables
+//Var
+
+var numberOne = 3;
+
+//const
+const numberTwo = 5;
+const PI =3.14;
+
+console.log(PI);
+
+//Let
+let numberThree = 9;
+
+//console.log(PI);
+function checkLet(){
+    let  numberFour =10;
+    console.log(numberFour);
+}
+console.log(numberThree);
+//checkLet();
+
+//Let
+ const array =[1,2,3,4,5,7,8,9]
+ //console.log();
+
+ function checkList(){
+    let number1 = 3;
+    console.log(number1);
+ }
+
+
+
