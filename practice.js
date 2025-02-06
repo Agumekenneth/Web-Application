2f7be68b9a9eb20f557f01fef47de4c042d1af0e
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-02-06 10:45:44 +0300

    Password.js

--- practice.js
+++ practice.js
@@ -15,3 +15,17 @@ const welcome = (username)=>{
     console.log(`welcome back,${username}`);
 }
 welcome("kenneth")
+
+let age = 20;
+if(age <= 18){
+    console.log("minor");
+}
+else if(age >=18){
+    console.log("adult");
+}
+let password = '2544';
+let confirmPassord = '2544'
+while(confirmPassord != password){
+    console.log("The user is valid")
+    break;
+}
